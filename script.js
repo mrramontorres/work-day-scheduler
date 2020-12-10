478d55a7cbb1d922a1f5a0cc0ad8d780076a052f
turtles <me@ramon.nyc>
2020-12-09 20:36:55 -0800

    added length of hours array after confirmation.

--- script.js
+++ script.js
@@ -3,22 +3,21 @@ document.getElementById("currentDay").innerHTML = moment().format("dddd, MMMM Do
 
 // Sets current hour
 var hourNow = moment().format("hA");
-console.log(hourNow);
+console.log("current time is: " + hourNow);
 
 // Array holds all of the hours available
 var hoursList = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM"];
-
+console.log("hoursList array index read is: " + hoursList.length);
 
 //Research the below code from: https://www.youtube.com/watch?v=zjYgz50E0mA&ab_channel=SteveGriffith
+// Function that initiizes the creation of new timeblock divs
 const init = function(){
     let t1
     t1 = document.getElementById("target1");
-
     div1 = document.querySelector(".timeBlockDiv");
-    for(let i=0; i<5; i++){
+    for(let i=0; i<hoursList.length-1; i++){
         t1.appendChild(div1.cloneNode(true));
     }
-
 }
 
 document.addEventListener("DOMContentLoaded", init)
