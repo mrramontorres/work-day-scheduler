cd9d9a6578ec12b3807cf29e732f61b38fe09a3b
turtles <me@ramon.nyc>
2020-12-10 22:59:57 -0800

    my brain is fried.

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 document.getElementById("currentDay").innerHTML = moment().format("dddd, MMMM Do");
 
 // TODO fix hour proccessing
-var hourNow = 12; 
-// var hourNow = moment().format("H");
+//var hourNow = 12; 
+var hourNow = moment().format("H");
 console.log("Current time variable is: " + hourNow);
 
 // Array holds all of the hours available
-var hoursList = ["9","10","11","12","13","14","15","16","17","18"];
+var hoursList = [9,10,11,12,13,14,15,16,17,18];
 //console.log("hoursList array index read is: " + hoursList.length);
 
 //Research the below code from: https://www.youtube.com/watch?v=zjYgz50E0mA&ab_channel=SteveGriffith
@@ -23,57 +23,63 @@ const init = function(){
         // Adds the appropriate hour from the hourArray
         //console.log(hoursList[i]);
         t2.querySelector("span").innerHTML = hoursList[i];
-            /* TEST TIME CONSOLE
+        var previousText = localStorage.getItem(hoursList[i]);
+            if(previousText !== null){
+                t2.getElementsByClassName("corn")[0].value = previousText;
+            }
+            /*
+
             console.log("Current hour: " + hourNow);
-            console.log("past: " + (hourNow > hoursList[i])); //past
-            console.log("present: " + (hourNow == hoursList[i])); //present
-            console.log("future: " + (hourNow < hoursList[i])); //future
+            console.log("future: " + (hourNow < hoursList[i]));
+            console.log("present: " + (hourNow == hoursList[i]));
+            console.log("past: " + (hourNow > hoursList[i]));
             */
+           console.log(hourNow);
+           console.log(hoursList[i]);
        // Adds the approprite color coding for the time baesd on current hour of the day
-        if(hourNow < hoursList[i]) {
-            t2.querySelector(".time").classList.add("future");
+        if(hourNow > hoursList[i]) {
+            t2.querySelector(".time").classList.add("past");
+            console.log('print1');
         }else if (hourNow == hoursList[i]) {
             t2.querySelector(".time").classList.add("present");
-        }else if (hourNow > hoursList[i]){
-            t2.querySelector(".time").classList.add("past");
-        };
-        console.log(t2);
-
+            console.log('print2');
+        }else if (hourNow < hoursList[i]){
+            t2.querySelector(".time").classList.add("future");
+            console.log('print3');
+        } else {
+            console.log("print4");
+        }
+        //console.log(t2);
     }
     //TODO move this from javascript to style sheet
     div1.style.display = "none";
 
 }
 
-function saveText(){
-    alert("something was clicked");
+//Listens for any saveBtn element to be clicked
+function saveText(x){
+    //testing function
+    var help = x.parentNode;
+    //console.log(x);
+    //console.log(x.parentNode);
+    //console.log(help);
+    //console.log(help.getElementsByClassName("corn")[0]);
+    var newText =  help.getElementsByClassName("corn")[0].value;
+    //console.log(newText);
+    //console.log(help);
+    //console.log(help.getElementsByClassName("thisISit")[0].innerHTML);
+    var thisIStime = help.getElementsByClassName("thisISit")[0].innerHTML;
+    
+    localStorage.setItem(thisIStime, newText);
+
 }
 
-
-//TODO listen for a save button click
-
-//TODO after click select textarea to save in local storage
-
-//TODO capture any text
 
 //Not sure I think this waits until the page content has loaded
 document.addEventListener("DOMContentLoaded", init)
 
+/*
 
-
-//CODE NOT USED
-/*$.each(hoursList, function(i){
-    console.log(i);
-    /*var newRowDiv = $( ".row" ).clone();
-    console.log(newRowDiv);
-    htmlDiv = "<div id='row'>" + hoursList[i] + "</div>";
-    console.log(hoursList[i]);
-    $(".container").append(htmlDiv);
-    
-
-               console.log("the if statment is: " + (hourNow < hoursList[i]));
-            t2.className("time","past");
-
-
-});*/
-
+    var lastNewText = JSON.parse(localStorage.getItem("newText"));
+    myText.innerHTML = lastNewText.newText;
+    */
